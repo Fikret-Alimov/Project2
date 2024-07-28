a1dcbf50c47978ec62d5ba0158ae95e47df3d7a9
Fikret-Alimov <137065671+Fikret-Alimov@users.noreply.github.com>
2024-07-28 07:42:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,10 @@ if (error.response) {
 res.status(500).send('Something went wrong!');
 }
 });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
-console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
